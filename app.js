72031e7ae68e9c7e53d21d15d43f84c0551b0fde
tweekdev <theauantoine@theau.local>
2020-09-01 19:00:27 +0200

    adding express

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const userName = 'Max';
-
-fs.writeFile('user-data.txt', 'Name: ' + userName, (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('WROTE FILE');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.post('/', (req, res) => {
+  return res.send('<h1>' + req.body.username + '</h1>');
 });
+
+app.get('/', (req, res, next) => {
+  res.send(
+    '<form method="POST"><input type="text" name="username"><button type="submit">send</button></form>'
+  );
+});
+
+app.listen(5000);
